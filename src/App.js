0a64e79413baa7493dc94ead81f4311a1f86ba73
Geetha9-ssb <geethakami133@gmail.com>
2024-10-28 15:55:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Myapp from './component/Myapp';
+import weather from './component/Weather';
+import Weather from './component/Weather';
+
+function App() {
+  return (
+   <>
+   <Myapp/>
+  
+   </>
+  );
+}
+
+export default App;
